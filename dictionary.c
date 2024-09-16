070a51614b73a38a4bc4fc48ea20c8fe5a850a0f
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-09-16 17:09:31 +0300

    Implement load function

--- dictionary.c
+++ dictionary.c
@@ -2,21 +2,15 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
 
-// Represents a node in a hash table
-typedef struct node
-{
-    char word[LENGTH + 1];
-    struct node *next;
-} node;
-
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
-
-// Hash table
-node *table[N];
+// Global variables
+node *trie; // the root node of the trie
+int dicwords = 0; // the number of words in the dictionary
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -28,15 +22,43 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
+    if (word[0] == '\'')
+    {
+        return 26;
+    }
+
     return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *dicfile = fopen(dictionary, "r");
+
+    if (dicfile == NULL)
+    {
+        return false;
+    }
+
+    trie = create_node();
+
+    // Read each word from the dictionary
+    char word[LENGTH + 1];
+    while (fgets(word, LENGTH + 1, dicfile))
+    {
+        // Remove the '\n' character from word
+        word[strcspn(word, "\n")] = '\0';
+
+        // Skip empty lines
+        if (word[0] != '\0')
+        {
+            dicwords++;
+
+            add_word(trie, word);
+        }
+    }
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
@@ -51,4 +73,43 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+// Creates a new trie node
+node *create_node(void)
+{
+    node *p = malloc(sizeof(node));
+    p->end_of_word = false;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        p->next[i] = NULL;
+    }
+
+    return p;
+}
+
+// Adds a new word to the trie
+void add_word(node *root, char *word)
+{
+    node *p = root;
+
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        // Create a string which contains just the word[i] character
+        char ch[2];
+        ch[0] = word[i];
+        ch[1] = '\n';
+
+        int index = hash(ch);
+
+        if (p->next[index] == NULL)
+        {
+            p->next[index] = create_node();
+        }
+
+        p = p->next[index];
+    }
+
+    p->end_of_word = true;
 }
--- dictionary.h
+++ dictionary.h
@@ -9,11 +9,24 @@
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
+// Defines the size of the alphabet
+// A-Z letters and apostroph character
+#define ALPHABET_SIZE 27
+
+// Represents a node in a trie
+typedef struct node
+{
+    struct node *next[ALPHABET_SIZE + 1];
+    bool end_of_word;
+} node;
+
 // Prototypes
 bool check(const char *word);
 unsigned int hash(const char *word);
 bool load(const char *dictionary);
 unsigned int size(void);
 bool unload(void);
+node *create_node(void);
+void add_word(node *root, char *word);
 
 #endif // DICTIONARY_H
